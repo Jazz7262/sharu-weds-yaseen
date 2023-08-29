5f1595d1cffbc7e07a367753fabe58d788dee50f
ziauddinziya07 <ziauddinziya07@gmail.com>
2023-08-29 09:05:42 +0530

    updated developer info

--- src/components/DeveloperInfo.jsx
+++ src/components/DeveloperInfo.jsx
@@ -56,7 +56,7 @@ function DeveloperInfo() {
                 <p>
                     ziauddinziya07@gmail.com
                     <br />
-                    +91 95737 67548.
+                    +91 85550 92440.
                 </p>
 
                 <div className="icon-section">
